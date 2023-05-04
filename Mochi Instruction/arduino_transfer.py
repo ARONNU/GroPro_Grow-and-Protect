e773aaf8ce737ff8abaaf39592f05da5ecaee652
Aronn <56037739+ARONNU@users.noreply.github.com>
2023-05-04 10:38:23 +0800

    Update arduino_transfer.py

--- Mochi Instruction/arduino_transfer.py
+++ Mochi Instruction/arduino_transfer.py
@@ -2,12 +2,14 @@ import serial
 import time
 
 with open('counter.txt', 'r') as file:
-    value1 = int(file.read().strip())
+    vis_count = int(file.read().strip())
 
 with open('sound_count.txt', 'r') as file:
-    value2 = int(file.read().strip())
+    aud_count = int(file.read().strip())
 
-value_send = value1 + value2
+if vis_count >= 25 & <= 40:
+
+
 
 # configure serial port
 ser = serial.Serial('/dev/ttyUSB0', 9600) # replace '/dev/ttyUSB0' with the appropriate serial port
@@ -18,3 +20,6 @@ while True:
     ser.write(str(value_send).encode()) # send data via serial port
     send_count += 1
     time.sleep(1) # wait for 1 second before sending next message
+
+
+#edit code from https://rimstar.org/science_electronics_projects/raspberry_pi_to_arduino_serial_usb_communication.htm
