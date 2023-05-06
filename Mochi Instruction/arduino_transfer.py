359fbcd6084878082fb27f93e581422094e651ee
Aronn <56037739+ARONNU@users.noreply.github.com>
2023-05-06 16:56:42 +0800

    Update arduino_transfer.py

--- Mochi Instruction/arduino_transfer.py
+++ Mochi Instruction/arduino_transfer.py
@@ -17,7 +17,7 @@ ser = serial.Serial('/dev/ttyUSB0', 9600) # replace '/dev/ttyUSB0' with the appr
 # send data to Arduino
 send_count = 0
 while True:
-    ser.write(str(value_send).encode()) # send data via serial port
+    ser.write(str(visual).command()) # send data via serial port
     send_count += 1
     time.sleep(1) # wait for 1 second before sending next message
 
